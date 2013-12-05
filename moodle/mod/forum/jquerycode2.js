ca039456ad5ca074f4393b08a8bfbf342c330bdc
viraajn <viraajn@gmail.com>
2013-12-04 17:48:55 -0700

    Travis' code

--- moodle/mod/forum/jquerycode2.js
+++ moodle/mod/forum/jquerycode2.js
@@ -1,4 +1,4 @@
-var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
+//var script = document.createElement('script');script.src = "https://ajax.googleapis.com/ajax/libs/jquery/1.6.3/jquery.min.js";document.getElementsByTagName('head')[0].appendChild(script);
 
 var x = parseInt($(".posting p")[0].innerHTML);
 var y = parseInt($( "input[name='countr']" ).val());
